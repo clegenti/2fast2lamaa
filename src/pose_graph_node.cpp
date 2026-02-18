9a0141898386ddd14eee46f76b82d23473851b17
Cedric Le Gentil <le.gentil.cedric@gmail.com>
2026-02-18 08:37:15 +0100

    Create images of submaps

--- src/pose_graph_node.cpp
+++ src/pose_graph_node.cpp
@@ -10,6 +10,11 @@
 
 #include "ffastllamaa/msg/submap_info.hpp"
 
+#include <opencv2/opencv.hpp>
+
+const float kCapHeight = 0.5; // Maximum height to consider for the submap images
+
+
 class PoseGraphNode: public rclcpp::Node
 {
     public:
@@ -38,6 +43,8 @@ class PoseGraphNode: public rclcpp::Node
 
     private:
         double free_space_carving_radius_ = -1.0;
+        double image_res_ = 0.5;
+        double map_res_ = 0.15;
 
         std::vector<std::string> map_paths_;
         std::vector<std::string> traj_files_;
@@ -45,14 +52,19 @@ class PoseGraphNode: public rclcpp::Node
         std::string output_folder_ = "";
 
         std::vector<std::shared_ptr<MapDistField> > maps_;
+        std::vector<std::pair<int64_t, int64_t> > map_time_ranges_;
+        std::vector<Mat4> submap_original_poses_;
+        std::vector<Mat2_3> cam_mats_;
 
         MapDistFieldOptions map_options_;
 
-        std::vector<std::shared_ptr<Vec7> > poses_;
-        ankerl::unordered_dense::map<int64_t, std::shared_ptr<Vec7> > time_to_pose_map_;
+        std::map<int64_t, std::shared_ptr<Vec7> > time_and_pose_;
+        int64_t last_odometry_time_ = -1;
+        bool has_been_gravity_aligned_ = false;
 
         // Add the storage of the submap images
-        
+        std::vector<cv::Mat> submap_images_;
+
 
         rclcpp::Subscription<ffastllamaa::msg::SubmapInfo>::SharedPtr sub_;
 
@@ -66,6 +78,8 @@ class PoseGraphNode: public rclcpp::Node
             if(scans_folder_ == "" && msg->scan_folder != "")
             {
                 scans_folder_ = msg->scan_folder;
+                image_res_ = msg->map_res*2.5;
+                map_res_ = msg->map_res;
             }
             if(map_options_.cell_size < 0.0)
             {
@@ -84,7 +98,9 @@ class PoseGraphNode: public rclcpp::Node
             std::vector<std::pair<int64_t, Vec7> > trajectory = readTrajectoryFile(msg->traj_file);
             addSubmapTrajectoryToState(trajectory);
 
-
+            map_time_ranges_.emplace_back(trajectory.front().first, trajectory.back().first);
+            submap_original_poses_.emplace_back(posQuatToTransform(trajectory.front().second));
+            cam_mats_.emplace_back(Mat2_3::Zero());
 
             if(free_space_carving_radius_ > 0.0)
             {
@@ -93,9 +109,61 @@ class PoseGraphNode: public rclcpp::Node
                 maps_.back()->writeMap(msg->ply_file);
                 RCLCPP_INFO(this->get_logger(), "Cleaned map saved to: %s", msg->ply_file.c_str());
             }
+
+            Vec3 gravity = Vec3(msg->gravity[0], msg->gravity[1], msg->gravity[2]);
+            if(!has_been_gravity_aligned_ && gravity.norm() > 1.0)
+            {
+                alignGravity(gravity);
+                has_been_gravity_aligned_ = true;
+            }
+
+            // Create the submap image
+            cv::Mat image = getSubmapImage(maps_.size() - 1);
+            submap_images_.push_back(image);
+            std::cout << "Created submap image for map " << maps_.size() - 1 << std::endl;
+
             writeFullTrajectory();
         }
 
+
+        void alignGravity(const Vec3& gravity)
+        {
+            // Align the gravity vector with the Z-axis
+            Vec3 z_axis(0.0, 0.0, 1.0);
+            Vec3 gravity_normalized = gravity.normalized();
+            Vec3 rotation_axis = gravity_normalized.cross(z_axis);
+            double angle = std::acos(gravity_normalized.dot(z_axis));
+            if(rotation_axis.norm() < 1e-6)
+            {
+                if(gravity_normalized.dot(z_axis) < 0)
+                {
+                    // Gravity is opposite to Z-axis, rotate 180 degrees around X-axis
+                    rotation_axis = Vec3(1.0, 0.0, 0.0);
+                    angle = M_PI;
+                }
+                else
+                {
+                    rotation_axis = Vec3(0.0, 0.0, 1.0); // No rotation needed, but we need a valid axis
+                    angle = 0.0;
+                }
+            }
+            else
+            {
+                rotation_axis.normalize();
+            }
+            Mat3 rotation_matrix = expMap(rotation_axis * angle);
+
+            // Apply the rotation to all poses in the state
+            Mat4 transform = Mat4::Identity();
+            transform.block<3,3>(0,0) = rotation_matrix;
+
+            for(auto& [timestamp, pose_ptr] : time_and_pose_)
+            {
+                Mat4 pose_mat = posQuatToTransform(*pose_ptr);
+                Mat4 rotated_pose_mat = transform * pose_mat;
+                *pose_ptr = transformToPosQuat(rotated_pose_mat);
+            }
+        }
 
 
 
@@ -153,33 +221,35 @@ class PoseGraphNode: public rclcpp::Node
 
         void addSubmapTrajectoryToState(const std::vector<std::pair<int64_t, Vec7> >& trajectory)
         {
-            Vec7 last_pose;
-            int64_t last_timestamp = -1;
             for(size_t i = 0; i < trajectory.size(); i++)
             {
                 int64_t timestamp = trajectory[i].first;
                 Vec7 pose = trajectory[i].second;
 
-                if(time_to_pose_map_.find(timestamp) == time_to_pose_map_.end())
+                if(time_and_pose_.size() == 0)
                 {
-                    if(i > 0 && last_timestamp != -1)
-                    {
-                        Mat4 last_pose_mat = posQuatToTransform(last_pose);
-                        Mat4 new_pose_mat = posQuatToTransform(pose);
-                        Mat4 delta_mat = last_pose_mat.inverse() * new_pose_mat;
-
-                        poses_.emplace_back(std::make_shared<Vec7>(transformToPosQuat<double>(posQuatToTransform<double>(*time_to_pose_map_[last_timestamp]) * delta_mat)));
-                        time_to_pose_map_[timestamp] = poses_.back();
-                    }
-                    else
-                    {
-                        std::cout << "Adding initial pose i = " << i << " timestamp " << timestamp << std::endl;
-                        poses_.emplace_back(std::make_shared<Vec7>(pose));
-                        time_to_pose_map_[timestamp] = poses_.back();
-                    }
+                    std::cout << "Adding first pose with timestamp " << timestamp << std::endl;
+                    auto pose_ptr = std::make_shared<Vec7>(pose);
+                    time_and_pose_[timestamp] = pose_ptr;
+                    continue;
                 }
-                last_pose = pose;
-                last_timestamp = timestamp;
+                if(i == 0 && (time_and_pose_.find(timestamp) == time_and_pose_.end()))
+                {
+                    std::string error_message = "First pose of the submap trajectory has a timestamp that does not exist in the state. This means that the submaps do not overlap and that should not happen. Timestamp: " + std::to_string(timestamp);
+                    RCLCPP_ERROR(this->get_logger(), "%s", error_message.c_str());
+                    throw std::runtime_error(error_message);
+                }
+
+
+                if(time_and_pose_.find(timestamp) == time_and_pose_.end())
+                {
+                    Mat4 last_pose_mat = posQuatToTransform(trajectory[i-1].second);
+                    Mat4 new_pose_mat = posQuatToTransform(pose);
+                    Mat4 delta_mat = last_pose_mat.inverse() * new_pose_mat;
+
+                    auto pose = std::make_shared<Vec7>(transformToPosQuat<double>(posQuatToTransform<double>(*time_and_pose_[trajectory[i-1].first]) * delta_mat));
+                    time_and_pose_[timestamp] = pose;
+                }
             }
             RCLCPP_INFO(this->get_logger(), "Added %zu poses to the state from trajectory.", trajectory.size());
         }
@@ -201,7 +271,7 @@ class PoseGraphNode: public rclcpp::Node
                 return;
             }
             file << "timestamp, x, y, z, rx, ry, rz" << std::endl; // Header
-            for (const auto& [timestamp, pose_ptr] : time_to_pose_map_)
+            for (const auto& [timestamp, pose_ptr] : time_and_pose_)
             {
                 Vec6 pos_rot_vec = posQuatToPosRot(*pose_ptr);
                 file << timestamp << ", "
@@ -213,7 +283,7 @@ class PoseGraphNode: public rclcpp::Node
                      << pos_rot_vec(5) << std::endl;
             }
             file.close();
-            RCLCPP_INFO(this->get_logger(), "Wrote full trajectory with %zu poses to file: %s", time_to_pose_map_.size(), full_traj_file.c_str());
+            RCLCPP_INFO(this->get_logger(), "Wrote full trajectory with %zu poses to file: %s", time_and_pose_.size(), full_traj_file.c_str());
         }
 
 
@@ -250,6 +320,118 @@ class PoseGraphNode: public rclcpp::Node
                 map->freeSpaceCarving(scan, pose_mat);
                 RCLCPP_INFO(this->get_logger(), "Carved free space from scan: %s", scan_file.c_str());
             }
+        }
+
+
+        cv::Mat getSubmapImage(int map_id)
+        {
+            std::shared_ptr<MapDistField> map = maps_[map_id];
+            std::vector<Pointd> pts = map->getPts();
+            if(pts.size() == 0)
+            {
+                RCLCPP_ERROR(this->get_logger(), "Map has no points. Cannot create submap image.");
+                throw std::runtime_error("Map has no points. Cannot create submap image.");
+            }
+
+            // Get the points in the reference frame of the first submap_pose
+            Mat4 first_submap_pose = submap_original_poses_[map_id];
+            Mat4 current_submap_pose = posQuatToTransform(*time_and_pose_[map_time_ranges_[map_id].first]);
+            Mat4 transform = current_submap_pose * first_submap_pose.inverse();
+            std::vector<Vec3> transformed_pts;
+            double min_x = std::numeric_limits<double>::max();
+            double max_x = std::numeric_limits<double>::lowest();
+            double min_y = std::numeric_limits<double>::max();
+            double max_y = std::numeric_limits<double>::lowest();
+            for(const auto& pt : pts)
+            {
+                Vec3 transformed_vec = transform.block<3,3>(0,0) * pt.vec3() + transform.block<3,1>(0,3);
+                transformed_vec(2) -= current_submap_pose(2,3); // Remove the height to create a 2D image
+                transformed_pts.emplace_back(transformed_vec(0), transformed_vec(1), transformed_vec(2));
+                min_x = std::min(min_x, transformed_vec(0));
+                max_x = std::max(max_x, transformed_vec(0));
+                min_y = std::min(min_y, transformed_vec(1));
+                max_y = std::max(max_y, transformed_vec(1));
+            }
+
+            // Create an image with the given resolution
+            int width = static_cast<int>(std::ceil((max_x - min_x) / image_res_))+1;
+            int height = static_cast<int>(std::ceil((max_y - min_y) / image_res_))+1;
+            MatX counter = MatX::Zero(height, width);
+            MatX sum_squared = MatX::Zero(height, width);
+            MatX sum = MatX::Zero(height, width);
+            cv::Mat image(height, width, CV_32F, cv::Scalar(0.0f));
+
+            Mat2_3 cam_mat;
+            cam_mat << 1.0/image_res_, 0.0, -min_x/image_res_,
+                            0.0, 1.0/image_res_, -min_y/image_res_;
+            cam_mats_[map_id] = cam_mat;
+
+            // Fill the image with the height values
+            for(const auto& pt : transformed_pts)
+            {
+                Vec2 cam_pt = cam_mat * Vec3(pt(0), pt(1), 1.0);
+                int x = static_cast<int>(std::floor(cam_pt(0)));
+                int y = static_cast<int>(std::floor(cam_pt(1)));
+                if(x >= 0 && x < width && y >= 0 && y < height)
+                {
+                    counter(y, x) += 1.0f;
+                    sum(y, x) += pt(2);
+                    sum_squared(y, x) += pt(2) * pt(2);
+                }
+            }
+            for(int y = 1; y < height-1; ++y)
+            {
+                for(int x = 1; x < width-1; ++x)
+                {
+                    int local_count = 0;
+                    double local_sum = 0;
+                    if(counter(y, x) == 0)
+                    {
+                        continue;
+                    }
+                    if(counter(y-1, x) > 0)
+                    {
+                        local_count++;
+                        local_sum += sum(y-1, x) / counter(y-1, x);
+                    }
+                    if(counter(y+1, x) > 0)
+                    {
+                        local_count++;
+                        local_sum += sum(y+1, x) / counter(y+1, x);
+                    }
+                    if(counter(y, x-1) > 0)
+                    {
+                        local_count++;
+                        local_sum += sum(y, x-1) / counter(y, x-1);
+                    }
+                    if(counter(y, x+1) > 0)
+                    {
+                        local_count++;
+                        local_sum += sum(y, x+1) / counter(y, x+1);
+                    }
+                    image.at<float>(y, x) = static_cast<float>((local_sum - local_count*sum(y, x) / counter(y, x))/(4.0*kCapHeight));
+                    if(image.at<float>(y, x) < -1.0f)
+                    {
+                        image.at<float>(y, x) = -1.0f;
+                    }
+                    else if(image.at<float>(y, x) > 1.0f)
+                    {
+                        image.at<float>(y, x) = 1.0f;
+                    }
+                }
+            }
+            // Apply a simple gaussian blur to smooth the image
+            cv::GaussianBlur(image, image, cv::Size(3, 3), 0, 0, cv::BORDER_DEFAULT);
+
+
+            // For debug, save the image
+            std::string image_file = output_folder_ + "/submap_image_" + std::to_string(map_id) + ".png";
+            cv::Mat debug_img;
+            image.convertTo(debug_img, CV_8U, 127.5, 127.5); // Scale to [0, 255] for visualization
+            cv::imwrite(image_file, debug_img);
+            std::cout << "Saved submap image to: " << image_file << std::endl;
+
+            return image;
         }
 
 
